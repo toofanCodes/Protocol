ee329a94a25b2ac128843b3d0aca12f6d05bcb77
toofanCodes <saran.in.usa@gmail.com>
2026-01-05 23:07:59 -0800

    feat: Create generic ShareSheet component

---
+++ Protocol/Views/Components/ShareSheet.swift
@@ -0,0 +1,18 @@
+//
+//  ShareSheet.swift
+//  Protocol
+//
+
+import SwiftUI
+import UIKit
+
+struct ShareSheet: UIViewControllerRepresentable {
+    let activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
